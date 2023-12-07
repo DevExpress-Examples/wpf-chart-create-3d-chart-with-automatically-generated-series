5027b86e161f3b54adf7592acdc667295b8bd230
DevExpressExampleBot <fakeEmail>
2023-12-07 18:59:30 +0400

    Source auto update [skip ci]

--- VB/Chart3D_Lesson3/Iris.vb
+++ VB/Chart3D_Lesson3/Iris.vb
@@ -1,56 +1,65 @@
-﻿Namespace Chart3D_Lesson3
-	Public Class IrisData
-		Private privateSpecies As String
-		Public Property Species() As String
-			Get
-				Return privateSpecies
-			End Get
-			Private Set(ByVal value As String)
-				privateSpecies = value
-			End Set
-		End Property
-		Private privateSepalWidth As Double
-		Public Property SepalWidth() As Double
-			Get
-				Return privateSepalWidth
-			End Get
-			Private Set(ByVal value As Double)
-				privateSepalWidth = value
-			End Set
-		End Property
-		Private privateSepalLength As Double
-		Public Property SepalLength() As Double
-			Get
-				Return privateSepalLength
-			End Get
-			Private Set(ByVal value As Double)
-				privateSepalLength = value
-			End Set
-		End Property
-		Private privatePetalWidth As Double
-		Public Property PetalWidth() As Double
-			Get
-				Return privatePetalWidth
-			End Get
-			Private Set(ByVal value As Double)
-				privatePetalWidth = value
-			End Set
-		End Property
-		Private privatePetalLength As Double
-		Public Property PetalLength() As Double
-			Get
-				Return privatePetalLength
-			End Get
-			Private Set(ByVal value As Double)
-				privatePetalLength = value
-			End Set
-		End Property
-		Public Sub New(ByVal species As String, ByVal sepalWidth As Double, ByVal sepalLength As Double, ByVal petalWidth As Double, ByVal petalLength As Double)
-			Me.Species = species
-			Me.SepalWidth = sepalWidth
-			Me.SepalLength = sepalLength
-			Me.PetalWidth = petalWidth
-			Me.PetalLength = petalLength
-		End Sub
-	End Class
+Namespace Chart3D_Lesson3
+
+    Public Class IrisData
+
+        Private _Species As String, _SepalWidth As Double, _SepalLength As Double, _PetalWidth As Double, _PetalLength As Double
+
+        Public Property Species As String
+            Get
+                Return _Species
+            End Get
+
+            Private Set(ByVal value As String)
+                _Species = value
+            End Set
+        End Property
+
+        Public Property SepalWidth As Double
+            Get
+                Return _SepalWidth
+            End Get
+
+            Private Set(ByVal value As Double)
+                _SepalWidth = value
+            End Set
+        End Property
+
+        Public Property SepalLength As Double
+            Get
+                Return _SepalLength
+            End Get
+
+            Private Set(ByVal value As Double)
+                _SepalLength = value
+            End Set
+        End Property
+
+        Public Property PetalWidth As Double
+            Get
+                Return _PetalWidth
+            End Get
+
+            Private Set(ByVal value As Double)
+                _PetalWidth = value
+            End Set
+        End Property
+
+        Public Property PetalLength As Double
+            Get
+                Return _PetalLength
+            End Get
+
+            Private Set(ByVal value As Double)
+                _PetalLength = value
+            End Set
+        End Property
+
+        Public Sub New(ByVal species As String, ByVal sepalWidth As Double, ByVal sepalLength As Double, ByVal petalWidth As Double, ByVal petalLength As Double)
+            Me.Species = species
+            Me.SepalWidth = sepalWidth
+            Me.SepalLength = sepalLength
+            Me.PetalWidth = petalWidth
+            Me.PetalLength = petalLength
+        End Sub
+    End Class
 End Namespace
